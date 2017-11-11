6623d7e402d5560d89eff98ff85d1df1e430131b
Zoe Dyksman <zbybee@hotmail.com>
2017-11-11 14:49:32 -0700

    finished portfolio

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+// hangman link
+	$("#hangman").on("click", function() {
+		window.location = "https://zdyk2003.github.io/Hangman-Game/";
+	});
+// eat-da-burger link
+	$("#burger").on("click", function() {
+		window.location = "https://stark-citadel-80828.herokuapp.com/";
+	});
+// eat-da-burger link
+	$("#trivia").on("click", function() {
+		window.location = "https://zdyk2003.github.io/TriviaGame/";
+	});
+// eat-da-burger link
+	$("#gif").on("click", function() {
+		window.location = "https://zdyk2003.github.io/Gif-Tastic/";
+	});
+// eat-da-burger link
+	$("#friend-finder").on("click", function() {
+		window.location = "https://arcane-island-60887.herokuapp.com/";
+	});
+
+
+});
