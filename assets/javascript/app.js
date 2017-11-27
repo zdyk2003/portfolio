244dd5cb19e33c0dee94e580ae52562d0b6c720d
Zoe Dyksman <zbybee@hotmail.com>
2017-11-26 19:21:11 -0700

    porfolio changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,27 +2,27 @@ $(document).ready(function(){
 
 // hangman link
 	$("#hangman").on("click", function() {
-		window.location = "https://zdyk2003.github.io/Hangman-Game/";
+		window.open("https://zdyk2003.github.io/Hangman-Game/", "_blank");
 	});
 // eat-da-burger link
 	$("#burger").on("click", function() {
-		window.location = "https://stark-citadel-80828.herokuapp.com/";
+		window.open("https://stark-citadel-80828.herokuapp.com/", "_blank");
 	});
 // eat-da-burger link
 	$("#trivia").on("click", function() {
-		window.location = "https://zdyk2003.github.io/TriviaGame/";
+		window.open("https://zdyk2003.github.io/TriviaGame/", "_blank");
 	});
 // eat-da-burger link
 	$("#gif").on("click", function() {
-		window.location = "https://zdyk2003.github.io/Gif-Tastic/";
+		window.open("https://zdyk2003.github.io/Gif-Tastic/", "_blank");
 	});
 // friend finder link
 	$("#friend-finder").on("click", function() {
-		window.location = "https://arcane-island-60887.herokuapp.com/";
+		window.open("https://arcane-island-60887.herokuapp.com/", "_blank");
 	});
 // slpm webpage link
 	$("#slpm").on("click", function() {
-		window.location = "https://zdyk2003.github.io/slpmWebpage/";
+		window.open("https://zdyk2003.github.io/slpmWebpage/", "_blank");
 	});
 
 
